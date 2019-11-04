566f710c80fc4f272278e18b76bd9931a2a7d67c
mihaly044 <mihaly044@gmail.com>
2019-11-04 14:30:57 +0100

    Added project files

---
+++ ElectronCGIDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ElectronCgi.DotNet;
+
+namespace ElectronCGIDemo
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var connection = new ConnectionBuilder()
+                .WithLogging()
+                .Build();
+
+            // expects a request named "greeting" with a string argument and returns a string
+            connection.On<string, string>("greeting", name => "Hello " + name);
+
+            // wait for incoming requests
+            connection.Listen();        
+        }
+    }
+}
